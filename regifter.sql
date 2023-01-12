26aed96caf58b9abf8b535c71281a1b5ecf3526b
Lennie Nurse <lnursej@gmail.com>
2023-01-12 11:15:37 -0500

    "update"

--- regifter.sql
+++ regifter.sql
@@ -76,7 +76,7 @@ SELECT gift FROM gifts WHERE  gift LIKE '%candle';
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
-SELECT * FROM gifts WHERE giver = 'Santa' OR price is >= 30;
+SELECT * FROM gifts WHERE giver = 'Santa' OR price > 30;
 
 --
 \echo Query for every gift whose giver is NOT Santa
