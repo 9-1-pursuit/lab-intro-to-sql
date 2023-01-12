c51fc97a0853953c95cdc5cefb53f19eed81599f
Lennie Nurse <lnursej@gmail.com>
2023-01-12 11:27:53 -0500

    "update"

--- regifter.sql
+++ regifter.sql
@@ -110,7 +110,7 @@ SELECT * FROM gifts;
 --
  \echo Count the total number of gifts that have the word candle in it
 -- 
-SELECT (*)COUNT FROM gifts WHERE LIKE '%candle%';
+SELECT (*)COUNT FROM gifts WHERE gift LIKE '%candle%';
 
 --
 \echo Get the AVEREAGE value from all the gifts
