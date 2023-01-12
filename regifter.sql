c1e31ec81b041c0de20f7a4c1168e9060195b1bb
TonyH98 <tonyhoang@pursuit.org>
2023-01-12 10:51:13 -0500

    Update query added

--- regifter.sql
+++ regifter.sql
@@ -102,6 +102,7 @@ UPDATE gifts SET price = 2999 WHERE id = 2 RETURNING *;
 \echo Query for the updated item
 --
 
+SELECT * FROM gifts WHERE id = 2;
 
 --
 \echo Delete all the gifts from Santa and return the 'value' and 'gift' of the gift you have deleted
